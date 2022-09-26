6b3be9294b9c8ea336aa44bde0423bca7ca5de0d
Adam Baty <adambaty@outlook.com>
2022-09-25 21:10:12 -0400

    WIP: Working on landing page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,46 @@ function App() {
         <h1>
           Adam Baty
         </h1>
+      </header>
+      <body className="App-body">
+        <h3>
+          Software Engineer
+        </h3>
         <h5>
           Bowling Green State University | Computer Science - Business Systems | Graduated December 2022
         </h5>
+        <h6>
+          Bio:
+        </h6>
         <p>
-          text about me goes here
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis nisl sed dui sollicitudin consectetur. Proin ultricies orci augue, quis facilisis elit aliquam vel. Phasellus faucibus, purus vitae pulvinar commodo, 
+          felis massa dictum nisi, eget sollicitudin dui nulla sit amet lectus. In hac habitasse platea dictumst. Suspendisse ac ullamcorper mi. In sollicitudin, nulla vel ultrices fringilla, tellus magna molestie tellus, 
+          ut condimentum elit dolor facilisis ipsum. Phasellus lectus ante, varius vel rutrum eu, convallis at nisl. Maecenas nec mi vel purus lacinia sodales. Donec in rhoncus nunc, at auctor mauris. Praesent condimentum faucibus rhoncus. 
+          In ornare eros id diam tempor hendrerit.
         </p>
-      </header>
+        <p>
+          Phasellus eu ante vel urna congue varius at at felis. Aliquam eros orci, mollis et lectus non, dignissim fringilla elit. Nulla pellentesque sem vitae sollicitudin lacinia. Donec eget auctor enim. Pellentesque nisl odio, 
+          volutpat cursus pretium vel, pulvinar vitae lectus. Suspendisse purus eros, vulputate non nisi non, porta tempus diam. Praesent pharetra viverra velit, ac sagittis mi dictum condimentum. Praesent et euismod quam. 
+          Aliquam tincidunt vulputate nunc quis bibendum. Aliquam enim magna, porttitor porttitor sagittis ut, aliquet eu risus. Etiam et ipsum id tortor lacinia interdum. Pellentesque vitae accumsan tellus. Fusce vitae dui enim. 
+          Quisque sodales lorem eu velit euismod, quis commodo sapien tempus. Donec semper posuere vestibulum.
+        </p>
+        <p>
+          Ut in tortor eu felis hendrerit consectetur. Morbi nec mollis turpis. Vivamus eu enim libero. Morbi hendrerit id ante fermentum viverra. Phasellus porttitor, purus id maximus sagittis, enim dolor blandit mauris, quis rutrum metus urna eu urna. 
+          Quisque venenatis tellus nec diam rutrum tincidunt. Pellentesque blandit eget felis ac eleifend. Aliquam sapien ipsum, interdum a diam non, pulvinar mattis mi. Proin cursus, tellus a gravida elementum, magna quam ultricies risus, 
+          in ornare dolor metus et dolor.
+        </p>
+        <p>
+          Vestibulum ornare massa ac felis ultricies, in cursus mi pellentesque. Nam sed iaculis nisi. Suspendisse lacinia nulla sem, ac sagittis nunc lacinia id. Sed auctor, massa non sagittis semper, enim nibh porttitor felis, 
+          ut consectetur dui ipsum at orci. Nulla vel lacus et augue fringilla facilisis ac et tellus. Curabitur non odio nunc. Integer sagittis at urna eu bibendum. Sed in lorem augue. Nunc sed maximus nisl.
+        </p>
+        <p>
+          Suspendisse iaculis enim id metus rhoncus gravida. Cras hendrerit est justo, et vehicula elit consequat ac. Vestibulum vehicula risus vel nibh interdum posuere. Maecenas porta ante nulla, in gravida arcu dictum eu. 
+          Morbi posuere nibh arcu, non imperdiet nisi commodo at. Phasellus purus purus, blandit sed mi at, sodales suscipit dolor. Pellentesque et lacinia dolor. 
+        </p>
+      </body>
+      <footer className="App-footer">
+
+      </footer>
     </div>
   );
 }

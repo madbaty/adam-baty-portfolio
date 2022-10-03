7fa3654d8362f871df15ed418e8a0f6d69884576
Adam Baty <adambaty@outlook.com>
2022-10-03 19:49:12 -0400

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://adambaty.com',
+    site: 'https://www.adam-baty.com',
 });
